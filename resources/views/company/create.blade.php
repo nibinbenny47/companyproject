048e4910150f6af071f15d0899d3604e99e71cf2
Nibin <nibinbenny47@gmail.com>
2023-09-23 16:31:23 +0530

    Employees edit and delete completed

--- resources/views/company/create.blade.php
+++ resources/views/company/create.blade.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
+    @include('header')
+
     <h1>Company registration</h1>
     <form action="/newCompanies" method="post">
         {{ csrf_field() }}
@@ -15,16 +19,16 @@
                 <td>Name</td>
                 <td><input type="text" name="name" value="{{old('name')}}"></td>
             </tr>
-            
+
             <tr>
-            <td>Email</td>
+                <td>Email</td>
                 <td><input type="text" name="email" value="{{old('email')}}"></td>
             </tr>
             <tr>
-            <td>Website</td>
+                <td>Website</td>
                 <td><input type="text" name="website" value="{{old('website')}}"></td>
             </tr>
-           
+
             <tr>
                 <td>
                     <button>Register</button>
@@ -33,4 +37,5 @@
         </table>
     </form>
 </body>
+
 </html>
